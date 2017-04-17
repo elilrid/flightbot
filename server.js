ad7780e53d569d295cf3f32f0554137aa349e1de
Ilkan Engin <ilkan.engin@gmail.com>
2017-04-17 23:20:49 +0300

    implemented the part for facebook messenger, in order to send message when a message comes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var fs = require("fs");
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/webhook', function(req, res) {
+app.get('/flights', function(req, res) {
   /*
   if (req.query['hub.mode'] === 'subscribe' &&
       req.query['hub.verify_token'] === VALIDATION_TOKEN) {
@@ -30,7 +30,7 @@ app.get('/webhook', function(req, res) {
  * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
  *
  */
-app.post('/webhook', function (req, res) {
+app.post('/flights', function (req, res) {
   var data = req.body;
 
   // Make sure this is a page subscription
@@ -44,6 +44,9 @@ app.post('/webhook', function (req, res) {
       // Iterate over each messaging event
       pageEntry.messaging.forEach(function(messagingEvent) {
         console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+        if (messagingEvent.message) {
+          receivedMessage(messagingEvent);
+        }
         /*if (messagingEvent.optin) {
           receivedAuthentication(messagingEvent);
         } else if (messagingEvent.message) {
@@ -70,17 +73,107 @@ app.post('/webhook', function (req, res) {
   }
 });
 
-app.get('/getFlights', function (req, res) {
-   //res.status(code || 500).json({"error": message});
-   fs.readFile( __dirname + "/" + "flights.json", 'utf8', function (err, data) {
-       console.log( data );
-       res.end( data );
-   });
-})
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+  console.log("Received message for user %d and page %d at %d with message:",
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
 
-app.post('/postFlights', function (req, res) {
-   res.status(200).json({"success": true});
-})
+  var isEcho = message.is_echo;
+  var messageId = message.mid;
+  var appId = message.app_id;
+  var metadata = message.metadata;
+
+  // You may get a text or attachment but not both
+  var messageText = message.text;
+  //var messageAttachments = message.attachments;
+  var quickReply = message.quick_reply;
+
+  if (isEcho) {
+    // Just logging message echoes to console
+    console.log("Received echo for message %s and app %d with metadata %s",
+      messageId, appId, metadata);
+    return;
+  } else if (quickReply) {
+    var quickReplyPayload = quickReply.payload;
+    console.log("Quick reply for message %s with payload %s",
+      messageId, quickReplyPayload);
+
+    sendTextMessage(senderID, "Quick reply tapped");
+    return;
+  }
+
+  if (messageText) {
+        sendTextMessage(senderID,"Searching for available flights according to given parameters!");
+        return;
+  }
+  else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+/*
+ * Send a text message using the Send API.
+ *
+ */
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText,
+      metadata: "DEVELOPER_DEFINED_METADATA"
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+/*
+ * Call the Send API. The message data goes in the body. If successful, we'll
+ * get the message id in a response
+ *
+ */
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      if (messageId) {
+        console.log("Successfully sent message with id %s to recipient %s",
+          messageId, recipientId);
+      } else {
+      console.log("Successfully called Send API for recipient %s",
+        recipientId);
+      }
+    } else {
+      console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+    }
+  });
+}
+
+// app.get('/getFlights', function (req, res) {
+//    //res.status(code || 500).json({"error": message});
+//    fs.readFile( __dirname + "/" + "flights.json", 'utf8', function (err, data) {
+//        console.log( data );
+//        res.end( data );
+//    });
+// })
+//
+// app.post('/postFlights', function (req, res) {
+//    res.status(200).json({"success": true});
+// })
 
 app.get('/policy', function (req, res) {
    res.render('policy');
