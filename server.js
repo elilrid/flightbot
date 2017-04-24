4217283db12a274984a8c125b96c5eacc8856127
Ilkan Engin <ilkan.engin@gmail.com>
2017-04-24 20:15:51 +0300

    added some logs just for fun

--- server.js
+++ server.js
@@ -284,6 +284,7 @@ const actions = {
         .then(function(next, err, detailInfo) {
           console.log("detailInfo : " + detailInfo);
           if (detailInfo == "") {
+            console.log("if detailInfo is not null");
             context.noFlight = true;
             delete context.foundFlights;
             //when everything is OK, clean up data
@@ -292,6 +293,7 @@ const actions = {
             delete context.date;
 
           } else {
+            console.log("else detailInfo is null");
             delete context.noFlight;
             context.foundFlights = '\nFlights from ' + departureCode + " to " + arrivalCode + " on " + formatDate(new Date(date)) + "\n" + detailInfo; // we should call a weather API here
             //when everything is OK, clean up data
@@ -302,7 +304,7 @@ const actions = {
           //return context;
           next();
         });
-
+      console.log("finished, returning context");
       return context;
     } else {
       return context;
