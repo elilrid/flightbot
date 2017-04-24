2539cc2960b5a9fddfcbb4b9f2d5bff2af2800c2
Rıdvan SIRMA <sirmar@itu.edu.tr>
2017-04-25 01:41:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -254,6 +254,22 @@ function formatFlightMessage(flightInfo) {
     } else {
       toReturn += "Not a Direct Flight";
     }
+     var segments = [quote.OutboundLeg, quote.InboundLeg].map(function (segment, index) {
+
+                    var departPlace = _.filter(data.Places, { PlaceId: segment.OriginId })[0];
+                    var arrivePlace = _.filter(data.Places, { PlaceId: segment.DestinationId })[0];
+                    var carriers = segment.CarrierIds.map(c => _.filter(data.Carriers, { CarrierId: c })[0].Name);
+
+                    return {
+                        group: index + 1,
+                        departAirport: { code: departPlace.IataCode, name: departPlace.Name },
+                        arriveAirport: { code: arrivePlace.IataCode, name: arrivePlace.Name },
+                        departCity: { code: departPlace.CityId, name: departPlace.CityName },
+                        arriveCity: { code: arrivePlace.CityId, name: arrivePlace.CityName },
+                        departTime: segment.DepartureDate,
+                        carriers: carriers
+                    };
+                });
 
     toReturn += " - ";
     toReturn += "Time : " + formatDate(new Date(quote.QuoteDateTime));
